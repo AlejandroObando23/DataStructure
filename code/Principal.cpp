d8198f0b8bc88b81f503a8499712f98e97b350b8
Mateo Medranda <matemedranda15@gmail.com>
2024-06-04 14:52:25 -0500

    1.5.0

--- code/Principal.cpp
+++ code/Principal.cpp
@@ -1,5 +1,9 @@
-#include <isotream>
+#include <iostream>
 #include <string.h>
+#include <conio.h>
+#define ANSI_BACKGROUND_BLUE "\033[44m"
+#define ANSI_BACKGROUND_RESET "\033[0m"
+
 using namespace std;
 
 struct Persona{
@@ -15,16 +19,69 @@ struct Turno{
     Turno* siguiente;
 };
 
+int escogerOpcion();
+void mostrarMenuPrincipal(int opcionActual);
+
 int main(){
 
-    do{
-        cout << "\t=== Bienvenido al sistema de Turnos ===" << endl;
-        cout << "Seleccione una opción:" << endl;
-        cout << "1. Agregar Turno" << endl;
-        cout << "2. Modificar Turno" << endl;
-        cout << "3. Registrar Asistencia" << endl;
-        cout << "4. Salir" << endl;
-    }while();
+    bool salir = false;
+    int opcion;
 
+    do{
+        opcion = escogerOpcion();
+
+        switch(opcion){
+
+        }
+
+    }while(!salir);
+
+    return 0;
 }
 
+int escogerOpcion(){
+	int opcionActual=1;
+	while(1){
+		system("cls");
+		mostrarMenuPrincipal(opcionActual);
+		char tecla =_getch();
+		switch(tecla){
+			case 72: opcionActual=(opcionActual>1)?opcionActual-1:4; break;
+			case 80: opcionActual=opcionActual<4?opcionActual+1:1; break;
+			case 13:system("cls");
+					switch(opcionActual){
+						case 1: return 1;break;
+						case 2: return 2;break;
+						case 3: return 3;break;
+						case 4: return 4;break;
+					};break;
+			}
+	}
+}
+
+void mostrarMenuPrincipal(int opcionActual){
+    cout << "\t\t=== Bienvenido al sistema de Turnos ===" << endl;
+    cout << "\n\tSeleccione una opcion:\n" << endl;
+
+    for(int i=1;i<=4;i++){
+    	if(i==opcionActual){
+    		printf("%s",ANSI_BACKGROUND_BLUE);
+		}
+		else{
+			printf("%s",ANSI_BACKGROUND_RESET);
+		}
+
+		switch(i){
+			case 1: cout << "\t1. Agregar Turno" << endl;
+                break;
+			case 2: cout << "\t2. Modificar Turno" << endl;
+                break;
+			case 3: cout << "\t3. Registrar Asistencia" << endl;
+                break;
+			case 4: cout << "\t4. Salir" << endl;
+                break;
+			}
+	}
+	printf("%s",ANSI_BACKGROUND_RESET);
+	cout << "\t==============" << endl;
+}
