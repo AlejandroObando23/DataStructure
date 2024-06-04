e540100d0fc12a8ee62c20716493443f0d13dc1e
unknown <matemedranda15@gmail.com>
2024-06-04 09:20:06 -0500

    1.3.0

--- code/Principal.cpp
+++ code/Principal.cpp
@@ -15,3 +15,16 @@ struct Turno{
     Turno* siguiente;
 };
 
+int main(){
+
+    do{
+        cout << "\t=== Bienvenido al sistema de Turnos ===" << endl;
+        cout << "Seleccione una opción:" << endl;
+        cout << "1. Agregar Turno" << endl;
+        cout << "2. Modificar Turno" << endl;
+        cout << "3. Registrar Asistencia" << endl;
+        cout << "4. Salir" << endl;
+    }while();
+
+}
+
