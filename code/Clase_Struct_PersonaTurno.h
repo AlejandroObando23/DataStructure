419908b3100bdff073cd3f55e0228256d0ae19dd
AlejandroObando23 <alejoobando21@gmail.com>
2024-06-10 17:41:17 -0500

    1.11.0

---
+++ code/Clase_Struct_PersonaTurno.h
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+struct Persona
+{
+    string id;
+    string nombre;
+    string apellido;
+    Persona* sig;
+};
+class ClasePersona
+{
+private:
+    Persona* cabeza;
+public:
+
+    ClasePersona():cabeza(NULL) {}
+
+    ~ClasePersona()
+    {
+        while(cabeza != NULL)
+        {
+            Persona* temp= cabeza;
+            cabeza=cabeza->sig;
+            delete temp;
+
+        }
+
+    }
+
+
+};
+struct Turno
+{
+    int horaInicio;
+    int horaFinal;
+    Persona doctor;
+    Turno* sig;
+};
+class ClaseTurno
+{
+};
+
